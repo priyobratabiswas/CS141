90536f423e9ccc1ea4cd940d375ee5d3d7727ccf
priyobratabiswas <42151072+priyobratabiswas@users.noreply.github.com>
2018-08-19 08:41:17 +0530

    Update lab3_q2.cpp

--- lab3_q2.cpp
+++ lab3_q2.cpp
@@ -2,19 +2,26 @@
 #include<iostream>
 #include<string>
 using namespace std;
+// introduce the main function
 int main ()
 {
+	// create a program which asks user's name and welcomes him
 	string name;
 	cout<<"Hello, what's your name?"<<endl;
 	cin>>name;
 	cout<<"welcome "<<name<<endl;
 	cout<<"lets get started with some arithmetics"<<endl;
+	//make a program using arithmetic functions for integers
+	// introduce the variables
 	int a,b;
 	cout<<"let's get started using some integers"<<endl;
 	cout<<"enter two integers";
+	//get input in the variables
 	cin>>a;
 	cin>>b;
+	// introduce the functions using the variables
 	int sum=a+b,subtraction1=a-b,subtraction2=b-a,multiplication=a*b,quotient1=a/b,quotient2=b/a,remainder1=a%b,remainder2=b%a;
+	// display the results using variable inputs in the arithmetic functions
 	cout<<"the summation of the two intergers is :"<<sum<<endl;
 	cout<<"the subtraction result when the second integer is subtracted from the first :"<<subtraction1<<endl;
 	cout<<"the subtraction result when the first integer is subtracted from the second :"<<subtraction2<<endl;
@@ -23,6 +30,8 @@ int main ()
 	cout<<"The quotient when the second integers gets divided by the first :"<<quotient2<<endl;
 	cout<<"The remainder when the first integers gets divided by the second :"<<remainder1<<endl;
 	cout<<"The remainder when the second integers gets divided by the first :"<<remainder2<<endl;
+	// make a program using arithmetic functions for small decimal numbers
+	// follow the same steps the as previous program but using float function instead of int
 	float c,d;
 	cout<<"let's get started using some small decimal numbers"<<endl;
 	cout<<"enter two small decimal numbers"<<endl;
@@ -35,6 +44,8 @@ int main ()
 	cout<<"The result when the two numbers are multiplied :"<<fmultiplication<<endl;
 	cout<<"The quotient when the first number gets divided by the second :"<<fquotient1<<endl;
 	cout<<"The quotient when the second number gets divided by the first :"<<fquotient2<<endl;
+	// make a program using arithmetic functions for small decimal numbers
+	// follow the same steps the as previous program but using double function instead of float
 	double e,f;
 	cout<<"let's get started using some big decimal numbers"<<endl;
 	cout<<"enter two big decimal numbers"<<endl;
@@ -47,5 +58,6 @@ int main ()
 	cout<<"The result when the two numbers are multiplied :"<<dmultiplication<<endl;
 	cout<<"The quotient when the first number gets divided by the second :"<<dquotient1<<endl;
 	cout<<"The quotient when the second number gets divided by the first :"<<dquotient2<<endl;
+	// return the main function
 return 0;
 }
